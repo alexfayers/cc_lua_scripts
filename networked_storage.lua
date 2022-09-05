972e51ff710c4f5661a5bd57a1988e98b216aea5
alexfayers <alex@fayers.com>
2022-09-06 00:20:13 +0100

    all check

--- networked_storage.lua
+++ networked_storage.lua
@@ -181,7 +181,7 @@ end
 
 function publicPullFromStorage(search_term, search_requested_count)
     local item_count = getStorageItemCount(search_term)
-    if item_count >= search_requested_count then
+    if item_count == "all" or item_count >= search_requested_count then
         print("Pulling " .. search_requested_count .. " out of " .. item_count .. " '".. search_term .."' from storage")
         pullItemsFromStorage(search_term, search_requested_count)
     else
