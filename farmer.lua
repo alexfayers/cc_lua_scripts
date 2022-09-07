338f7b5388f9c3b9e74f0937baf6516535fe5558
alexfayers <alex@fayers.com>
2022-09-07 20:31:02 +0100

    update test

--- farmer.lua
+++ farmer.lua
@@ -253,6 +253,14 @@ function test()
     moveTo(0, 0, 0)
     moveTo(0, 0, 2)
     moveTo(0, 0, 0)
+
+    moveTo(-2, 0, 0)
+    moveTo(0, 0, 0)
+    moveTo(0, -2, 0)
+    moveTo(0, 0, 0)
+    moveTo(0, 0, -2)
+    moveTo(0, 0, 0)
+
     log("Position: " .. state.position.x .. ", " .. state.position.y .. ", " .. state.position.z)
 
     -- moveTo(0, 0, 0)
