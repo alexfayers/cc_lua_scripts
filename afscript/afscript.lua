216061b96cfed3a0032d2977b9c3d068f5da4fcf
alexfayers <alex@fayers.com>
2022-09-11 22:12:41 +0100

    add script manager

---
+++ afscript/afscript.lua
@@ -0,0 +1,61 @@
+-- imports
+
+local completion = require "cc.completion"
+
+-- Update the updater script
+local file_url = "https://raw.githubusercontent.com/alexfayers/cc_lua_scripts/main/afscript/meta/update.lua"
+local file_contents = http.get(file_url).readAll()
+local file = fs.open("afscript/meta/update.lua", "w")
+file.write(file_contents)
+file.close()
+
+local update = require("afscript.meta.update")
+
+-- Define autocomplete for the updater
+
+function complete(shell, index, argument, previous)
+    if index == 1 then
+        return completion.choice(argument, {"update"}, true)
+    elseif index == 2 then
+        if previous[#previous] == "update" then
+            return completion.choice(argument, {"library", "script"}, true)
+        end
+    end
+end
+
+-- Help menu for the updater
+
+local function help()
+    print("Usage: " .. arg[0] .. " <command> [options]")
+    print("Commands:")
+    print(" " .. arg[0] .. " update library")
+    print(" " .. arg[0] .. " update script <script_name> [<script_name_2>, etc.]")
+end
+
+-- Handle updates
+local args = {...}
+
+if #args == 0 then
+    help()
+else
+    if args[1] == "update" then
+        if args[2] == "library" then
+            update.update_library()
+        elseif args[2] == "script" then
+            if args[3] == nil then
+                help()
+            else
+                for i = 3, #args do
+                    update.update_file(args[i])
+                end
+            end
+        else
+            help()
+        end
+    else
+        help()
+    end
+end
+
+-- Set the autocomplete
+shell.setCompletionFunction("afscript", complete)
---
+++ afscript/meta/update.lua
@@ -0,0 +1,73 @@
+-- Bootloader type script to fetch library files from github
+
+---Update an arbitrary script from the repo
+---@param script_name string The name of the script to update
+---@param options table|nil The options table
+local function _update_file(script_name, options)
+    assert(type(script_name) == "string", "script_name must be a string")
+    assert(type(options) == "nil" or type(options) == "table", "options must be a table when specified")
+
+    if options == nil then
+        options = {}
+    end
+
+    if options.auto_extension == nil then
+        options.auto_extension = true
+    end
+
+    if options.auto_extension == true then
+        -- if the script doesn't end with .lua, add it
+        if not script_name:match(".*lua$") then
+            script_name = script_name .. ".lua"
+        end
+    end
+
+    local script_url = "https://raw.githubusercontent.com/alexfayers/cc_lua_scripts/main/" .. script_name
+    local script_contents = http.get(script_url).readAll()
+
+    local file = fs.open(script_name, "w")
+    file.write(script_contents)
+    file.close()
+
+    print("Updater: Updated '" .. script_name .. "'")
+end
+
+---Fetch the latest afscript library files from github
+---@param options table|nil The options table
+local function _update_library(options)
+    assert(type(options) == "nil" or type(options) == "table", "options must be a table when specified")
+
+    if options == nil then
+        options = {}
+    end
+
+    local submodules = options.submodules or {
+        "core",
+        "turtle"
+    }
+
+    local tree_url = "https://api.github.com/repos/alexfayers/cc_lua_scripts/git/trees/main?recursive=1"
+    local tree = http.get(tree_url).readAll()
+    local tree_json = textutils.unserialiseJSON(tree)
+
+    for submodule_i = 1, #submodules do
+        print("Updater: Fetching submodule '" .. submodules[submodule_i] .. "'...")
+        for _, file in ipairs(tree_json.tree) do
+            if file.path:match("^afscript/" .. submodules[submodule_i] .. "/.+") then
+                _update_file(file.path, {auto_extension = false})
+                local file_url = "https://raw.githubusercontent.com/alexfayers/cc_lua_scripts/main/" .. file.path
+                local file_contents = http.get(file_url).readAll()
+
+                local file = fs.open(file.path, "w")
+                file.write(file_contents)
+                file.close()
+            end
+        end
+    end
+    print("Updater: Done!")
+end
+
+return {
+    update_library = _update_library,
+    update_file = _update_file
+}
