2a93ea77b49b43bf4444d424b35db0447c2d9382
alexfayers <alex@fayers.com>
2022-09-11 22:28:39 +0100

    functionalise

--- afscript/afman.lua
+++ afscript/afman.lua
@@ -13,7 +13,7 @@ local update = require("afscript.meta.update")
 
 -- Define autocomplete for the updater
 
-function complete(shell, index, argument, previous)
+local function complete(_, index, argument, previous)
     if index == 1 then
         return completion.choice(argument, {"update"}, true)
     elseif index == 2 then
@@ -34,33 +34,38 @@ end
 
 -- Handle CLI args
 
-if #arg == 0 then
-    help()
-else
-    if arg[1] == "update" then
-        if arg[2] == "library" then
-            update.update_library()
-        elseif arg[2] == "script" then
-            if arg[3] == nil then
-                print("Error: No script name provided")
+local function handleArgs()
+    if #arg == 0 then
+        help()
+    else
+        if arg[1] == "update" then
+            if arg[2] == "library" then
+                update.update_library()
+            elseif arg[2] == "script" then
+                if arg[3] == nil then
+                    print("Error: No script name provided")
+                    print()
+                    help()
+                else
+                    for i = 3, #arg do
+                        update.update_file(arg[i])
+                    end
+                end
+            else
+                print("Error: '" .. arg[2] .."' is not a valid option for 'update'")
                 print()
                 help()
-            else
-                for i = 3, #arg do
-                    update.update_file(arg[i])
-                end
             end
         else
-            print("Error: '" .. arg[2] .."' is not a valid option for 'update'")
+            print("Error: '" .. args[1] .. "' is not a valid command")
             print()
             help()
         end
-    else
-        print("Error: '" .. args[1] .. "' is not a valid command")
-        print()
-        help()
     end
 end
 
 -- Set the autocomplete
 shell.setCompletionFunction("afman.lua", complete)
+
+-- handle the args
+handleArgs()
