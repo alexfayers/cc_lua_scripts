10589159b2143d2294235a6cc615e2b526ae81ab
alexfayers <alex@fayers.com>
2022-09-17 21:25:39 +0100

    correctly set level

--- _test_notify.lua
+++ _test_notify.lua
@@ -3,6 +3,6 @@
 local notify = require("afscript.core.notify")
 local logging = require("afscript.core.logging")
 
-notify.logger.level = logging.DEBUG
+notify.logger.level = logging.LEVEL.DEBUG
 
 notify.join("Test", "This is a test notification")
