839a7b1018e270368e44e2e8daa5b52d0cabe50d
alexfayers <alex@fayers.com>
2022-09-19 15:00:55 +0100

    packet system

--- remote.lua
+++ remote.lua
@@ -1,5 +1,10 @@
 --- Enable remote control of a computer via rednet
 
+---imports
+
+local logging = require("afscript.core.logging")
+local logger = logging.logger("remote")
+
 ---Config
 
 -- A list of computers that are allowed to control this computer
@@ -24,24 +29,65 @@ local modem = {
 )}
 
 if not modem then
+    logger.error("No wireless modem found")
     error("No wireless modem found")
+end
+
+function buildPacket(command, args)
+    return textutils.serialize({
+        type = command,
+        args = args,
+    })
+end
+
+function handlePacket(packet)
+    local data = textutils.unserialize(packet)
+    local args = data.args
+    if data.type == "run" then
+        local result = shell.run(args.command)
+        if not result then
+            logger.error("Error running command")
+        else
+            logger.success("Command ran successfully")
+        end
+    end
 end
 
 function getCommand()
     while true do
-        local event, sender, message, protocol = os.pullEvent("rednet_message")
+        local _, sender, packet, protocol = os.pullEvent("rednet_message")
 
-        print(event, sender, message, protocol)
+        -- check if the message is from an allowed source
+
+        local allowed = false
+        for _, id in ipairs(allowed_sources) do
+            if id == sender then
+                allowed = true
+                break
+            end
+        end
+
+        if not allowed then
+            logger.warn("Received message from unallowed source: " .. sender)
+            return
+        end
 
         if protocol ~= nil and protocol == PROTOCOL_NAME then
-            print("Received message from " .. sender .. " with protocol " .. protocol .. " and message " .. tostring(message))
-        else
-            print("Received message from " .. sender .. " with message " .. tostring(message))
+            logger.info("Received message from " .. sender ": " .. tostring(command))
+            
+            handlePacket(packet)
         end
     end
 end
 
 
 function sendCommand(command, target)
-    rednet.send(target, command, PROTOCOL_NAME)
+    local payload = buildPacket(
+        "run",
+        {
+            command = command,
+        }
+    )
+
+    rednet.send(target, payload, PROTOCOL_NAME)
 end
